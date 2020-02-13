6ce9c2550590526156c5c545a324febb37ded9d6
Arnold <arniwebdev@gmail.com>
2020-02-13 22:14:16 +0200

    Add megoldas code to megoldas js, ups!

--- megoldas/megoldas.js
+++ megoldas/megoldas.js
@@ -4,3 +4,18 @@ import { beSzam, kiIr } from "../utils/utils.js";
 
 // Elágazás, if else
 // 03. Írjunk programot, amely kér egy számot a felhasználótól. Írja ki a szám hogy páros-e vagy sem. (Elágazás)
+
+// uzenunk a felhasznalonak, hogy kerunk egy szamot
+kiIr("Kerem a szamot!");
+// beolvassuk a szamot
+let szam = beSzam();
+// vizsgalju, hogy a szam paros-e
+if (szam % 2 == 0) {
+    // a szamunk paros
+    kiIr("A beolvasott szam: P A R O S!");
+} else {
+    // a szamunk paratlan
+    kiIr("A beolvasott szam: P A R A T L A N!");
+}
+// lefutott a program
+kiIr("Program vege!");
