bfc253eaff55ca43433577ac45d2a002dd9a24cc
Arnold <arniwebdev@gmail.com>
2020-02-15 14:43:00 +0200

    fix typo

--- app.js
+++ app.js
@@ -3,4 +3,4 @@ import { beSzam, kiIr } from "./utils/utils.js";
 // 👇 ide irhatod a kodod 👇
 
 // Elágazás, if else
-// 03. Írjunk programot, amely kér egy számot a felhasználótól. Írja ki a szám hogy páros-e vagy sem. (Elágazás)
+// 03. Írjunk programot, amely kér egy számot a felhasználótól. Írja ki, hogy a szám páros-e vagy sem. (Elágazás)
--- megoldas/megoldas.js
+++ megoldas/megoldas.js
@@ -3,7 +3,7 @@ import { beSzam, kiIr } from "../utils/utils.js";
 // 👇 itt a feladat megoldasa 👇
 
 // Elágazás, if else
-// 03. Írjunk programot, amely kér egy számot a felhasználótól. Írja ki a szám hogy páros-e vagy sem. (Elágazás)
+// 03. Írjunk programot, amely kér egy számot a felhasználótól. Írja ki, hogy a szám páros-e vagy sem. (Elágazás)
 
 // uzenunk a felhasznalonak, hogy kerunk egy szamot
 kiIr("Kerem a szamot!");
