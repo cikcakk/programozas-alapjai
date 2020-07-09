95cdde6718db17e81b17ef395fbb8adee9b69d29
Betty <netmadarasz@gmail.com>
2020-07-09 09:23:42 +0200

    mentes

--- app.js
+++ app.js
@@ -4,3 +4,17 @@ import { beSzam, kiIr } from "./utils/utils.js";
 
 // Math, a JavaScript beépített objektuma
 // 02. Írjunk programot, amely bekéri egy kocka oldalainak a hosszát. Írja ki a kocka alapterületét és a térfogatát!
+
+kiIr("kérem a kozka oldalának a hosszát!");
+
+let oldalHossza = beSzam();
+
+oldalHossza = Math.abs(oldalHossza);
+
+let alapTerulet = Math.pow(oldalHossza, 2);
+let terfogat = Math.pow(oldalHossza, 3);
+
+kiIr("a kozka alapterülete:");
+kiIr(alapTerulet);
+kiIr("a kocka terfogata:");
+kiIr(terfogat);
