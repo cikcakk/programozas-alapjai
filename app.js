862e34a25ce2cff26e9438d7e1ecc4eff1d51289
Betty <netmadarasz@gmail.com>
2020-07-09 09:32:00 +0200

    mentes

--- app.js
+++ app.js
@@ -4,3 +4,7 @@ import { beSzam, kiIr } from "./utils/utils.js";
 
 // Szekvencia: egymás utáni lépések
 //01. Írjunk programot, amely kér egy számot a felhasználótól. Írja ki a szám négyzetét.
+kiIr("kérem a szamot");
+let x = 0;
+x = beSzam();
+kiIr(x * x);
